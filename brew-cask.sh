c0abde11b1f8bd0ed912b82736a1172a51af77b0
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-03-31 06:45:35 +0200

    Added scripts for migrating to a new machine if needed

---
+++ brew-cask.sh
@@ -0,0 +1,22 @@
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install alfred
+brew cask install dropbox
+cask install flux
+
+# dev
+brew cask install iterm2
+brew cask install sublime-text
+
+# browsers
+brew cask install google-chrome-canary
+brew cask install firefox-nightly --force
+brew cask install webkit-nightly --force
+
+# less often
+brew cask install vlc
+brew cask install keycastr  
---
+++ brew.sh
@@ -0,0 +1,35 @@
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+# Install some other useful utilities like `sponge`
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed --default-names
+
+
+# Install Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install bash
+# regular bash-completion package is held back to an older release, so we get latest from versions.
+#   github.com/Homebrew/homebrew/blob/master/Library/Formula/bash-completion.rb#L3-L4
+brew tap homebrew/versions
+brew install homebrew/versions/bash-completion2
+
+# Install more recent versions of some OS X tools
+brew install vim --override-system-vi
+
+# Install other useful binaries
+brew install git
+
+# Remove outdated versions from the cellar
+brew cleanup
