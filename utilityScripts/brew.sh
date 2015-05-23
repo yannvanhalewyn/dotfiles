b21923ef30a19ebce1f08c2477c666063cebf25d
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-05-23 22:19:51 +0200

    Add new brews to brew file, using gnu coreutils, gnu sed, gnu grep now

--- utilityScripts/brew.sh
+++ utilityScripts/brew.sh
@@ -7,19 +7,22 @@ brew update
 brew upgrade --all
 
 # Install GNU core utilities (those that come with OS X are outdated)
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-#brew install coreutils
+brew install coreutils
 # Install some other useful utilities like `sponge`
 #brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 #brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`
-#brew install gnu-sed --default-names
+brew install gnu-sed --default-names
 
 
 # Install more recent versions of some OS X tools
-brew install vim --override-system-vi --with-lua
-brew install tmux
+brew tap neovim/neovim
+brew install --HEAD neovim
+brew tap homebrew/dupes
+brew install grep --with-default-names
+# brew install vim --override-system-vi --with-lua
+brew install tmux --with-nvim-tui-color
 brew install mysql
 brew install git
 brew install boost
@@ -28,9 +31,22 @@ brew install sfml
 brew install sdl2
 brew install sdl2_mixer
 brew install sdl2_ttf
-brew install mysql
-brew tap homebrew/versions
-brew install homebrew/versions/bash-completion2
+brew install cheat
+brew install cowsay
+brew install curlpp
+brew install figlet
+brew install imagemagick
+brew install lua
+brew install mongodb
+brew install mysql-connector-c++
+brew install node
+brew install python
+brew install qt
+brew install ag
+brew install tree
+brew install youtube-dl
+brew install zlib
+brew install webkit2png
 
 # setup for tmux connecting to OSX pastebin
 brew install reattach-to-user-namespace
