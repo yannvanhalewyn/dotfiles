c5e206d9ffb69f32152e86605e4aba890affa690
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-05-23 22:10:51 +0200

    Major update to symlink script, add zlogin with link

--- utilityScripts/symlinkAll.sh
+++ utilityScripts/symlinkAll.sh
@@ -1,21 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 ###########################################################
-# This script creates symlinks for every $file in $dir to ~
+# This script creates symlinks for my dotfiles to ~
 ###########################################################
 
-## VARIABLES
+# Variables
 dir=/Users/yannvanhalewyn/dotfiles
-files="bash_profile bash_prompt gitignore_global vimrc gvimrc gitconfig profile tmux.conf functions aliases osx vim nvim nvimrc zshrc cheat"
+files="bash_profile bash_prompt gitignore_global
+"
 
-## GOT TO DIR
+# Got to dir
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
-## DELETING .FILES AND CREATING SYMLINKS
+# Get files
+files=$(ls -A | grep -v 'DS_Store\|.git\|Scripts')
+echo "Will create symlinks for:"
+echo "$files"
+
+# Prompt user
+echo "Continue? (Y/n) \c"; read -n1 ans;
+if [ $ans != 'y' ]
+then
+  echo "\nOk! Some other time then."
+  exit
+fi
+
+# Deleting exising .links and creating symlinks
 for file in $files; do
-    echo "Deleting .$file"
-    rm ~/.$file
-    echo "Creating symlink for .$file"
+    echo "Deleting ~/.$file"
+    rm $HOME/.$file
+    echo "Linking $dir/$file -> ~/.$file"
     ln -s $dir/$file ~/.$file
 done
---
+++ zlogin
@@ -0,0 +1,12 @@
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+echo " __________
+< Hi there >
+ ----------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\\
+                ||----w |
+                ||     ||
+                  "
