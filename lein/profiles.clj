73d2ded212f6c878f15a243139c92142b02466e1
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-04-19 01:29:56 +0200

    Check in ~/.lein/profiles.clj and link it with ansible

---
+++ lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
+                       [acyclic/squiggly-clojure "0.1.9-SNAPSHOT" :exclusions [org.clojure/tools.reader]]]
+        :plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.17.0-SNAPSHOT"]]}}
