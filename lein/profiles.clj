240603fd1748b751e760040e9350cb08cc50c1bb
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-05-18 17:33:18 +0200

    Scope capture and snippets

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [acyclic/squiggly-clojure "0.1.9-SNAPSHOT" :exclusions [org.clojure/tools.reader]]]
+                       [vvvvalvalval/scope-capture "0.1.4"]
+                       [acyclic/squiggly-clojure "0.1.9-SNAPSHOT"
+                        :exclusions [org.clojure/tools.reader]]]
         :plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.17.0-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.17.0"]]
+        :injections [(require 'sc.api)]}}
