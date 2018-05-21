60479c37a282433dc8da87ff15ee097920b84c1d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-05-21 18:10:20 +0200

    Cider snapshot

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,5 +3,5 @@
                        [acyclic/squiggly-clojure "0.1.9-SNAPSHOT"
                         :exclusions [org.clojure/tools.reader]]]
         :plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.17.0"]]
+                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]]
         :injections [(require 'sc.api)]}}
