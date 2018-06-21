10b49582b1b46556dab33d872a5b59e4f532a99f
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-06-21 05:13:26 +0200

    And move last bits (like load path and init-functions) to litterate config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,17 +1,3 @@
-;;                          .         .
-;; 8 8888888888            ,8.       ,8.                   .8.           ,o888888o.       d888888o.
-;; 8 8888                 ,888.     ,888.                 .888.         8888     `88.   .`8888:' `88.
-;; 8 8888                .`8888.   .`8888.               :88888.     ,8 8888       `8.  8.`8888.   Y8
-;; 8 8888               ,8.`8888. ,8.`8888.             . `88888.    88 8888            `8.`8888.
-;; 8 888888888888      ,8'8.`8888,8^8.`8888.           .8. `88888.   88 8888             `8.`8888.
-;; 8 8888             ,8' `8.`8888' `8.`8888.         .8`8. `88888.  88 8888              `8.`8888.
-;; 8 8888            ,8'   `8.`88'   `8.`8888.       .8' `8. `88888. 88 8888               `8.`8888.
-;; 8 8888           ,8'     `8.`'     `8.`8888.     .8'   `8. `88888.`8 8888       .8' 8b   `8.`8888.
-;; 8 8888          ,8'       `8        `8.`8888.   .888888888. `88888.  8888     ,88'  `8b.  ;8.`8888
-;; 8 888888888888 ,8'         `         `8.`8888. .8'       `8. `88888.  `8888888P'     `Y8888P ,88P'
-;;
-
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -22,6 +8,4 @@
   (interactive)
   (org-babel-load-file (concat user-emacs-directory "configuration.org")))
 
-(add-to-list 'load-path (concat user-emacs-directory "config"))
-(require 'init-functions)
 (load-config)
