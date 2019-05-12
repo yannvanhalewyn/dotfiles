989a09ec5ab7cb6c733bcc29fdea917e3271150a
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-05-12 23:25:35 +0200

    Update lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,9 @@
-{:user {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [vvvvalvalval/scope-capture "0.1.4"]
+{:user {:dependencies [[nrepl "0.6.0"]
+                       [vvvvalvalval/scope-capture "0.3.2"]
                        [acyclic/squiggly-clojure "0.1.9-SNAPSHOT"
                         :exclusions [org.clojure/tools.reader]]]
         :plugins [[refactor-nrepl "2.4.0"]
-                  [jonase/eastwood "0.2.9"]
-                  [lein-kibit "0.1.6"]
-                  [cider/cider-nrepl "0.18.0"]]
+                  [cider/cider-nrepl "0.21.1" ]]
+        :middleware [refactor-nrepl.plugin/middleware cider-nrepl.plugin/middleware]
         :injections [(require 'sc.api)]
         :env {:squiggly {:checkers [:eastwood :kibit]}}}}
