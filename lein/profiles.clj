860121397a762c3c68cdd15cad1563dbeb9b85c2
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-05-17 19:48:23 +0200

    Update cider-nrepl for lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,7 +3,7 @@
                        [acyclic/squiggly-clojure "0.1.9-SNAPSHOT"
                         :exclusions [org.clojure/tools.reader]]]
         :plugins [[refactor-nrepl "2.4.0"]
-                  [cider/cider-nrepl "0.21.1" ]]
+                  [cider/cider-nrepl "0.22.0-beta1" ]]
         :middleware [refactor-nrepl.plugin/middleware cider-nrepl.plugin/middleware]
         :injections [(require 'sc.api)]
         :env {:squiggly {:checkers [:eastwood :kibit]}}}}
