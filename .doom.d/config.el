8872380c967c4c447d2f3bfe2d4a6b2bd7479aad
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-03-25 13:06:18 +0100

    Fix consult line integration with evil-search

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -233,6 +233,25 @@
    (:map company-active-map
     "C-h" nil
     "C-d" 'company-show-doc-buffer)))
+(setq uniquify-buffer-name-style nil)
+
+(use-package! consult
+  :config
+  (setq consult-line-start-from-top t)
+
+  (defun noct-consult-line-evil-history (&rest _)
+    "Add latest `consult-line' search pattern to the evil search history ring.
+This only works with orderless and for the first component of the search."
+    (when (and (bound-and-true-p evil-mode)
+               (eq evil-search-module 'evil-search))
+      (let ((pattern (car (orderless-pattern-compiler (car consult--line-history)))))
+        (add-to-history 'evil-ex-search-history pattern)
+        (setq evil-ex-search-pattern (list pattern t t))
+        (setq evil-ex-search-direction 'forward)
+        (when evil-ex-search-persistent-highlight
+          (evil-ex-search-activate-highlight evil-ex-search-pattern)))))
+
+  (advice-add #'consult-line :after #'noct-consult-line-evil-history))
 
 (use-package! neotree
   :config
@@ -269,7 +288,6 @@
           "\\*Backtrace\\*"
           "\\*helpful.*"
           "\\*ert\\*"
-          "\\*ivy-occur.*"
           help-mode
           compilation-mode))
 
@@ -288,8 +306,8 @@
   (popper-mode +1)
   (popper-echo-mode +1))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Rust
-
-;; (setq rustic-lsp-server 'rust-analyzer)
+(use-package embark
+  :config
+  ;; Enable fuzzy search
+  ;; (setq orderless-matching-styles '(orderless-flex))
+  (setq orderless-matching-styles '(orderless-literal orderless-regexp)))
