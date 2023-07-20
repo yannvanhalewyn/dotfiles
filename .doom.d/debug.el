d2d82895606f536d8eef72048e350376faeb0134
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-07-20 22:42:20 +0200

    Working completion debug.el

---
+++ .doom.d/debug.el
@@ -0,0 +1,72 @@
+;;; ../dotfiles/.doom.d/minial.el -*- lexical-binding: t; -*-
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;; (setq package-selected-packages '(clojure-mode lsp-mode cider lsp-treemacs flycheck company))
+(setq package-selected-packages '(use-package evil))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basic settings
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-minimum-prefix-length 1
+      ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
+      ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
+      )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages
+
+(use-package clojure-mode :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :custom
+  ;; (lsp-lens-enable t)
+  ;; (lsp-semantic-tokens-enable t)
+)
+
+(add-hook 'clojure-mode-hook 'lsp)
+(add-hook 'clojurescript-mode-hook 'lsp)
+(add-hook 'clojurec-mode-hook 'lsp)
+
+(use-package cider :ensure t)
+(use-package lsp-treemacs :ensure t)
+(use-package flycheck :ensure t)
+
+(use-package evil
+  :bind (("C-." . completion-at-point))
+  :ensure t
+  :init
+  (evil-mode t))
+
+;; (use-package company
+;;   :diminish company-mode
+;;   ;; :init (global-company-mode)
+;;   :bind (:map company-active-map
+;;          ("<tab>" . company-select-next)
+;;          ("S-<tab>" . company-select-previous)
+;;          ("RET" . company-complete)
+;;          ("C-h" . nil)
+;;          ("C-d" . company-show-doc-buffer)
+;;          ("C-s" . company-filter-candidates)
+;;          ("C-n" . company-select-next)
+;;          ("C-p" . company-select-previous))
+;;   :config
+;;   (setq company-idle-delay 0
+;;         ;; company-require-match nil
+;;         ;; company-frontends '(company-tng-frontend
+;;         ;;                     company-pseudo-tooltip-frontend
+;;         ;;                     company-echo-metadata-frontend)
+;;         ))
